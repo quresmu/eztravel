08e5eea7545d06ca2f5e3eb741297abba4134c2f
quresmu <91107560+quresmu@users.noreply.github.com>
2024-12-11 18:47:38 +1100

    Update your-deploy-script.sh

--- your-deploy-script.sh
+++ your-deploy-script.sh
@@ -1,3 +1,3 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-nvm install node
-npm install --save-dev @datadog/datadog-ci
+sudo nvm install node
+sudo npm install --save-dev @datadog/datadog-ci
